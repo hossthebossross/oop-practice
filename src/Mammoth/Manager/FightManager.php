3536216c13cd4a6002f48bf4553003e00b833d58
hossthebossross <hossthebossross@gmail.com>
2013-08-17 02:07:39 -0500

    boulderdash

--- src/Mammoth/Manager/FightManager.php
+++ src/Mammoth/Manager/FightManager.php
@@ -9,16 +9,17 @@ class FightManager
 {
     public function fight(UnitInterface $unit1, UnitInterface $unit2)
     {
-        for $unit1 {
-            while ($health > 0) {
+        $unitX = ($unit1, $unit2)
+
+        foreach array($unitX) {
+            while ($health >= 0) {
             $health = ($health - ($attackDmg % $attackRate));
             } 
         }   
-        for $unit2 {
-            while ($health > 0) {
-            $health = ($health - ($attackDmg % $attackRate));
-            } 
-        }  
+        $winner = (($unit1, $unit2) ==> ($health >0));
+        
+        return $winner;
+
         // make these bitches fight to the death
         return 'no one won';// result of the fight
     }
