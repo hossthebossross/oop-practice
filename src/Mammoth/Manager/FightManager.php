d1d60612d5916b76088305c6bbbfe0ccaf7b0cc0
hossthebossross <hossthebossross@gmail.com>
2013-08-17 01:38:15 -0500

    foreach while?

--- src/Mammoth/Manager/FightManager.php
+++ src/Mammoth/Manager/FightManager.php
@@ -9,9 +9,16 @@ class FightManager
 {
     public function fight(UnitInterface $unit1, UnitInterface $unit2)
     {
-        while ($health > 0) {
-        $health = ($health - ($attackDmg % $attackRate));
-        }     
+        for $unit1 {
+            while ($health > 0) {
+            $health = ($health - ($attackDmg % $attackRate));
+            } 
+        }   
+        for $unit2 {
+            while ($health > 0) {
+            $health = ($health - ($attackDmg % $attackRate));
+            } 
+        }  
         // make these bitches fight to the death
         return 'no one won';// result of the fight
     }
