c02b747d79871359156dddffdc3db0e7816c489c
hossthebossross <hossthebossross@gmail.com>
2013-08-17 23:50:18 -0500

    division attack speed starting teams

--- src/Mammoth/Manager/FightManager.php
+++ src/Mammoth/Manager/FightManager.php
@@ -2,25 +2,28 @@
 
 namespace Mammoth\Manager;
 
-use Mammoth\Unit\UnitInterface;
+use Mammoth\team\UnitInterface;
        
 class FightManager
 {
-    public function fight(UnitInterface $unit1, UnitInterface $unit2)
+    public function fight(UnitInterface $team1, Unitinterface $team2)
     {
-        $unit1AttackDamage = $unit1->getAttackDmg();
-        $unit2AttackDamage = $unit2->getAttackDmg();
+        $team1attackdamage = $team1->getAttackDmg();
+        $team2AttackDamage = $team2->getAttackDmg();
+        $team1attackrate = $team1->getAttackRate();
+        $team2AttackRate = $team2->getAttackRate();
 
-        while (($unit1->getHealth() > 0) && ($unit2->getHealth() > 0)) {
-            $unit1->setHealth($unit1->getHealth() - $unit2AttackDamage);
-            $unit2->setHealth($unit2->getHealth() - $unit1AttackDamage);
+
+        while (($team1->getHealth() > 0) && ($team2->getHealth() > 0)) {
+            $team1->setHealth($team1->getHealth() - ($team2AttackDamage % $team2AttackRate));
+            $team2->setHealth($team2->getHealth() - ($team1attackdamage % $team1attackrate));
         }
 
-        $units = array($unit1, $unit2);
+        $teams = array($team1, $team2);
 
-        foreach ($units as $unit) {
-            if ($unit->getHealth() > 0) {
-                $winner = $unit;
+        foreach ($teams as $team) {
+            if ($team->getHealth() > 0) {
+                $winner = $team;
             }
         }
 
